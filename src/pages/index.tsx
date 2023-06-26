b12dd456aec67e322a639c72ba7f458d614029f7
Gérald Leban <leban@insa-toulouse.fr>
2023-06-26 22:39:47 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta name="description" content="" />
+      </Head>
+    </>
+  );
+}
