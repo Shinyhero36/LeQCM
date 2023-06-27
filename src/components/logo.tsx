a377a8d8d9d26273cc791bbc0b52da283edc1c08
Gérald LEBAN <gerald.leban0@gmail.com>
2023-06-28 00:21:50 +0200

    feat(landing-page): Design the landing page (#6)
    * feat(landing-page): Design the landing page
    
    * Change locale
    
    * Add canonical url

---
+++ src/components/logo.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+type Theme = "light" | "dark";
+
+export const Logo = ({ theme = "light" }: { theme: Theme }) => (
+  <div className="flex items-center gap-2">
+    <div
+      className={cn("h-6 w-6 rounded-full", {
+        "bg-black": theme === "light",
+        "bg-white": theme === "dark",
+      })}
+    />
+    <p
+      className={cn("text-xl", {
+        "text-black": theme === "light",
+        "text-white": theme === "dark",
+      })}
+    >
+      Le qcm
+    </p>
+  </div>
+);
