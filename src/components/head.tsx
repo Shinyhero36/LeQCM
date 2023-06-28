de2fdb29a9a59ceb8c69ab8a182dd9725f384ccd
Gérald Leban <leban@insa-toulouse.fr>
2023-06-28 09:13:38 +0200

    opg

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,41 +1,17 @@
 import NextHead from "next/head";
-
-const metatags = {
-  title: "Jumbio: Biological system predictive simulation tool",
-  description:
-    "A dynamic simulator: the course of several days long cultivations (in batch, fed-batch, chemostat, accelerostat, multi-stage, industrial scale bioreactors) is easily simulated within a few minutes.",
-  url: "https://...",
-  site_name: "Jumbio",
-  keywords: ["Jumbio", "Adenon"],
-  image: {
-    url: "",
-  },
-  facebook: {
-    image: "",
-  },
-  twitter: {
-    title: "Jumbio: Biological system predictive simulation tool",
-    description:
-      "A dynamic simulator: the course of several days long cultivations (in batch, fed-batch, chemostat, accelerostat, multi-stage, industrial scale bioreactors) is easily simulated within a few minutes.",
-    image: "",
-  },
-};
+import { metadata } from "@/config/meta";
 
 export const Head = () => (
   <NextHead>
     <meta charSet="utf-8" />
-    <title>{metatags.title}</title>
-    <meta name="description" content={metatags.description} />
-    <link rel="author" href="https://shadcn.com" />
-    {/* {metatags.author && (
-      <>
-        <meta name="author" content={metatags.author} />
-        <meta name="creator" content={metatags.author} />
-      </>
-    )} */}
-    {metatags.keywords && (
-      <meta name="keywords" content={metatags.keywords.join(",")} />
-    )}
+    {/* HTML Meta Tags */}
+    <title>{metadata.title}</title>
+    <meta name="description" content={metadata.description} />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="author" href={metadata.author.url} />
+    <meta name="author" content={metadata.author.name} />
+    <meta name="creator" content={metadata.creator} />
+    <meta name="keywords" content={metadata.keywords.join(",")} />
     <meta
       name="theme-color"
       media="(prefers-color-scheme: light)"
@@ -48,34 +24,22 @@ export const Head = () => (
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <meta property="og:title" content={metatags.title} />
-    <meta property="og:description" content={metatags.description} />
-    <meta property="og:url" content={metatags.url} />
-    <meta property="og:site_name" content={metatags.site_name} />
+    {/* Open Graph / Facebook */}
+    <meta property="og:title" content={metadata.title} />
+    <meta property="og:description" content={metadata.description} />
+    <meta property="og:url" content={metadata.url} />
+    <meta property="og:site_name" content={metadata.siteName} />
     <meta property="og:locale" content="en_UK" />
-    {metatags.image && (
-      <meta property="og:image" content={metatags.image.url} />
-    )}
+    <meta property="og:image" content={metadata.image} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
-    <meta property="og:image:alt" content={metatags.title} />
+    <meta property="og:image:alt" content={metadata.title} />
     <meta property="og:type" content="website" />
-    {metatags.twitter && (
-      <>
-        <meta name="twitter:card" content="summary_large_image" />
-        {/* {metatags.twitter.creator && (
-          <meta name="twitter:creator" content={metatags.twitter.creator} />
-        )} */}
-        <meta name="twitter:title" content={metatags.twitter.title} />
-        <meta
-          name="twitter:description"
-          content={metatags.twitter.description}
-        />
-        <meta name="twitter:image" content={metatags.twitter.image} />
-      </>
-    )}
-    <link rel="shortcut icon" href="/favicon-16x16.png" />
-    <link rel="icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+    {/* Open Graph / Twitter */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={metadata.title} />
+    <meta name="twitter:description" content={metadata.description} />
+    <meta name="twitter:image" content={metadata.image} />
   </NextHead>
 );
