2a608b7a6384a1b9876904c72ae2757bc0d55584
Gérald Leban <leban@insa-toulouse.fr>
2023-06-30 01:23:45 +0200

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,5 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
 
 export const config = {
-  /*
-   * Match all request paths except for the ones starting with:
-   * - api or trpc (API routes) => except for /api/og
-   * - _next/static (static files)
-   * - _next/image (image optimization files)
-   */
-  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(?!/og)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)((?!/og).*)"],
 };
