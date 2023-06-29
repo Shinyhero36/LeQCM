df864ddc30ab95f545d13c71be04000074662d5e
Gérald Leban <leban@insa-toulouse.fr>
2023-06-29 19:26:33 +0200

    create the waiting room (admin side)

---
+++ src/pages/play/[id].tsx
@@ -0,0 +1,9 @@
+export default function AdminPlayPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1>
+        Welcome to the waiting room, grab a tea the game will start soon...
+      </h1>
+    </div>
+  );
+}
