a3d0bfb4994150a221d3e74ead8b1c16c19d2ed7
Gérald LEBAN <gerald.leban0@gmail.com>
2023-06-29 20:41:52 +0200

    Admin side waiting room (#11)

--- src/components/quiz-metadata.tsx
+++ src/components/quiz-metadata.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import { type Quiz } from "@prisma/client";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -16,9 +17,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { api } from "@/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { TimerIcon } from "lucide-react";
+import { Edit2Icon, TimerIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -87,10 +89,96 @@ export const QuizMetadataCard = ({
       <div className="p-6">
         {/* Image placeholder */}
         <div className="aspect-video h-52 max-w-full rounded-lg bg-black"></div>
-        <p className="mt-4 text-xl font-medium">{quiz.name}</p>
-        <p className="mt-1 text-sm text-gray-700">
-          {quiz.description ?? "Pas de description"}
-        </p>
+        <div className="mt-4 flex">
+          <div className="flex-grow">
+            <p className="text-xl font-medium">{quiz.name}</p>
+            <p className="mt-1 text-sm text-gray-700">
+              {quiz.description ?? "Pas de description"}
+            </p>
+          </div>
+          <Dialog
+            open={open}
+            onOpenChange={(state) => {
+              if (!isLoading) {
+                setOpen(state);
+              }
+            }}
+          >
+            <DialogTrigger asChild>
+              <Button variant={"ghost"} className="h-9 w-9 p-0">
+                <Edit2Icon className="h-5 w-5" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Modifier les informations du quiz</DialogTitle>
+              </DialogHeader>
+              <Form {...form}>
+                <form
+                  autoComplete="off"
+                  onSubmit={form.handleSubmit((data) => {
+                    mutate({
+                      ...data,
+                      id: quiz.id,
+                    });
+                  })}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nom</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="timeToAnswer"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>
+                          Temps de réponse entre les questions
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            {...form.register("timeToAnswer", {
+                              valueAsNumber: true,
+                            })}
+                            type="number"
+                            min={0}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" disabled={isLoading}>
+                    Modifier
+                  </Button>
+                </form>
+              </Form>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
       <div className="space-y-4 px-6 pb-6">
         <div className="flex items-center gap-4">
@@ -104,86 +192,12 @@ export const QuizMetadataCard = ({
         </div>
       </div>
       <div className="px-6 pb-6">
-        <Dialog
-          open={open}
-          onOpenChange={(state) => {
-            if (!isLoading) {
-              setOpen(state);
-            }
-          }}
+        <Link
+          href={`/play/${quiz.id}`}
+          className={cn(buttonVariants(), "w-full")}
         >
-          <DialogTrigger asChild>
-            <Button className="w-full">Modifier</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Modifier les informations du quiz</DialogTitle>
-            </DialogHeader>
-            <Form {...form}>
-              <form
-                autoComplete="off"
-                onSubmit={form.handleSubmit((data) => {
-                  mutate({
-                    ...data,
-                    id: quiz.id,
-                  });
-                })}
-                className="space-y-4"
-              >
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Nom</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="timeToAnswer"
-                  render={() => (
-                    <FormItem>
-                      <FormLabel>
-                        Temps de réponse entre les questions
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          {...form.register("timeToAnswer", {
-                            valueAsNumber: true,
-                          })}
-                          type="number"
-                          min={0}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" disabled={isLoading}>
-                  Modifier
-                </Button>
-              </form>
-            </Form>
-          </DialogContent>
-        </Dialog>
+          Lancer une partie
+        </Link>
       </div>
     </div>
   );
