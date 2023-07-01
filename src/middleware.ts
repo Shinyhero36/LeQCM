ea5efb651bb1000fb4cc2939e6a03e1b3d319e47
Gérald Leban <leban@insa-toulouse.fr>
2023-07-01 10:15:06 +0200

    Add static opg image

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,31 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { authMiddleware, clerkClient, redirectToSignIn } from "@clerk/nextjs";
+
+type PublicMetadata = {
+  role?: "admin";
+  isInvited?: boolean;
+};
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/og.png"],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) return NextResponse.next();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    if (!auth.userId) return redirectToSignIn({ returnBackUrl: req.url });
+
+    if (auth.userId) {
+      const pubMeta = (await clerkClient.users.getUser(auth.userId))
+        .publicMetadata as PublicMetadata;
+
+      if (pubMeta.role === "admin" || pubMeta.isInvited)
+        return NextResponse.next();
+
+      // TODO: Replace with "/invite" and create the page
+      const inviteURL = new URL("/", new URL(req.url).origin);
+      return NextResponse.redirect(inviteURL);
+    }
+  },
 });
 
 export const config = {
