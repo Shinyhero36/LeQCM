6d3aa9163131b9cb7dfb0a756d1d33b217d0fc4e
Gérald Leban <leban@insa-toulouse.fr>
2023-07-02 16:43:27 +0200

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,17 +9,18 @@ type PublicMetadata = {
 export default authMiddleware({
   publicRoutes: ["/", "/og.png"],
   async afterAuth(auth, req) {
+    console.log("BASE URL", req.url);
     if (auth.isPublicRoute) return NextResponse.next();
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     if (!auth.userId) return redirectToSignIn({ returnBackUrl: req.url });
 
     if (auth.userId) {
-      const pubMeta = (await clerkClient.users.getUser(auth.userId))
-        .publicMetadata as PublicMetadata;
+      // const pubMeta = (await clerkClient.users.getUser(auth.userId))
+      // .publicMetadata as PublicMetadata;
 
-      if (pubMeta.role === "admin" || pubMeta.isInvited)
-        return NextResponse.next();
+      // if (pubMeta.role === "admin" || pubMeta.isInvited)
+      return NextResponse.next();
 
       // TODO: Replace with "/invite" and create the page
       // const url = req.nextUrl.clone();
