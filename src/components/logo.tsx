1377541a7e861436d62595ce4026de107e9e8d81
Gérald Leban <leban@insa-toulouse.fr>
2023-07-18 19:31:02 +0200

    .

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,22 +1,7 @@
-import { cn } from "@/lib/utils";
+import Link from "next/link";
 
-type Theme = "light" | "dark";
-
-export const Logo = ({ theme = "light" }: { theme: Theme }) => (
-  <div className="flex items-center gap-2">
-    <div
-      className={cn("h-6 w-6 rounded-full", {
-        "bg-black": theme === "light",
-        "bg-white": theme === "dark",
-      })}
-    />
-    <p
-      className={cn("text-xl font-medium", {
-        "text-black": theme === "light",
-        "text-white": theme === "dark",
-      })}
-    >
-      Le qcm
-    </p>
-  </div>
+export const Logo = ({ ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+  <Link href="/" className="self-center text-2xl font-semibold" {...props}>
+    Le QCM
+  </Link>
 );
